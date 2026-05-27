ad7c3151b1aa9fbee832af396d6732bfb1e55270
Voahary <voaharyradoniaina@gmail.com>
2026-05-27 08:40:55 +0300

    Gestion d'erreurs

--- assets/src/Abstracts/Document.php
+++ assets/src/Abstracts/Document.php
@@ -1,4 +1,34 @@
 <?php
 declare(strict_types=1);
 namespace assets\src\abstracts;
+
+abstract class Document 
+{
+    protected string $titre;
+    protected string $auteur;
+    protected string $anneePulication;
+
+    protected static int $nombreDocuments = 0;
+    
+    public function _construct(
+        string $titre,
+        string $auteur,
+        int $anneePulication
+    ) {
+        if (trim($titre) === '')
+            {
+                throw new \InvalidArgumentException("Le titre ne peut pas être vide.");
+            }
+
+            if (trim($auteur) === '')
+                {
+                    throw new \InvalidArgumentException("L'auteur ne peut pas être vide");
+                }
+                if ($anneePulication < 0)
+                    {
+                        throw new \InvalidArgumentException("Année de publication invalide");
+                    }
+                    
+    }
+}
 ?>
